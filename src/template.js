cd88eb3527b95e137dfd910d49e7936941e53842
Luis <luis.cabrera@gmail.com>
2021-04-18 12:19:31 -0500

    fix leak

--- src/template.js
+++ src/template.js
@@ -48,12 +48,13 @@ function generateHTML(fullTeam) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="luiscabrera77">
     <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
   <script src="https://kit.fontawesome.com/0f5da0b5c9.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="./assets/css/styles.css">
-    <title>My Team</title>
+    <title>Luis' Team</title>
   </head>
   <body>
   
